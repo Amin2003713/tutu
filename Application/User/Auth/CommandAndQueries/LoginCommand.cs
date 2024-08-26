7c12e95ef6b487d2fa22a71fb3e197dd107d44cb
amin1 <Amin1382422amin@gmail.com>
2024-08-26 07:13:36 +0330

    a

--- Application/User/Auth/CommandAndQueries/LoginCommand.cs
+++ Application/User/Auth/CommandAndQueries/LoginCommand.cs
@@ -1,3 +1,18 @@
-﻿namespace Application.User.Auth.CommandAndQueries;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record LoginCommand(string PhoneNumber, string Password);
+namespace Application.User.Auth.CommandAndQueries
+{
+    public class LoginCommand
+    {
+        [Required(ErrorMessage = "شماره تلفن الزامی است.")]
+        [Phone(ErrorMessage = "لطفاً یک شماره تلفن معتبر وارد کنید.")]
+        [Display(Name = "شماره تلفن")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "رمز عبور الزامی است.")]
+        [StringLength(100, ErrorMessage = "رمز عبور باید حداقل {2} و حداکثر {1} کاراکتر باشد.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "رمز عبور")]
+        public string Password { get; set; }
+    }
+}
