8089daa427d00220bb2f18923bbec2f660e14390
amin1 <Amin1382422amin@gmail.com>
2024-08-26 09:45:08 +0330

    wqads

--- Ui/Ui.Client/Program.cs
+++ Ui/Ui.Client/Program.cs
@@ -1,10 +1,8 @@
 using Application.Extensions;
 using Infra.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddMudServices();
 
 builder.Services.RegisterApplicationDependency();
 builder.Services.RegisterInfraDependency();
