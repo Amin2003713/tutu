7394f018d15e41177ad954a5138ffbb1fd4eb2d4
amin1 <Amin1382422amin@gmail.com>
2024-08-26 08:36:01 +0330

    aDDEDS forms

--- Application/User/Auth/CommandAndQueries/RegisterCommand.cs
+++ Application/User/Auth/CommandAndQueries/RegisterCommand.cs
@@ -1,9 +1,18 @@
-﻿namespace Application.User.Auth.CommandAndQueries;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.User.Auth.CommandAndQueries;
 
 public class RegisterCommand
 {
-    public string PhoneNumber { get; set; } 
-    public string Password { get; set; } 
-    public string ConfirmPassword { get; set; }
+    [Required(ErrorMessage = "وارد کردن شماره تلفن ضروری است.")]
+    [RegularExpression(@"^(\+98|0)?9\d{9}$", ErrorMessage = "شماره تلفن وارد شده معتبر نیست.")]
+    public string PhoneNumber { get; set; }
 
+    [Required(ErrorMessage = "وارد کردن رمز عبور ضروری است.")]
+    [StringLength(100, ErrorMessage = "رمز عبور باید حداقل {2} و حداکثر {1} کاراکتر باشد.", MinimumLength = 6)]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "تکرار رمز عبور ضروری است.")]
+    [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن مطابقت ندارند.")]
+    public string ConfirmPassword { get; set; }
 }
