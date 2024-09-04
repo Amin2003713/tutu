a02da160a01b8676c333ecf792713e224758962d
amin <amin1382422@gmail.com>
2024-09-04 19:34:41 +0330

    aSdadad

--- BlazorProjetc.UI/BlazorProjetc.UI.Client/Program.cs
+++ BlazorProjetc.UI/BlazorProjetc.UI.Client/Program.cs
@@ -1,5 +1,11 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Application.Extensions;
+using Blazored.LocalStorage;
+using BlazorProjetc.UI.Client.Common.Auth;
+using BlazorProjetc.UI.Client.Common.Services;
 using Infra.Extensions;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
@@ -7,6 +13,22 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.RegisterApplicationDependency();
 builder.Services.RegisterInfraDependency();
+
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
+
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<ILocalStorageService>();
+builder.Services.AddScoped<WebAssemblyLocalStorage>();
 
 builder.Services.AddMudServices();
 
--- BlazorProjetc.UI/BlazorProjetc.UI/Program.cs
+++ BlazorProjetc.UI/BlazorProjetc.UI/Program.cs
@@ -9,6 +9,7 @@ using Infra.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using MudBlazor;
 
 const string baseAddress = "https://localhost:5001/";
 
@@ -19,7 +20,22 @@ builder.Services.RegisterApplicationDependency();
 
 
 // Add MudBlazor services
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = true;
+    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 2;
+    config.SnackbarConfiguration.HideTransitionDuration = 5;
+    config.SnackbarConfiguration.ShowTransitionDuration = 5;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+
+    config.SnackbarConfiguration.MaxDisplayedSnackbars = 3;
+    config.SnackbarConfiguration.ClearAfterNavigation = true;
+    config.SnackbarConfiguration.BackgroundBlurred = true;
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
