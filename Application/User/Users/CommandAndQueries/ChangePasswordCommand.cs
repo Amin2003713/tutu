2acf6aae941aa22fb8c577f61190333d25d0597f
amin <amin1382422@gmail.com>
2024-10-06 12:36:43 +0330

    ascascsac

--- Application/User/Users/CommandAndQueries/ChangePasswordCommand.cs
+++ Application/User/Users/CommandAndQueries/ChangePasswordCommand.cs
@@ -1,8 +1,17 @@
 ﻿namespace Application.User.Users.CommandAndQueries;
 
+using System.ComponentModel.DataAnnotations;
+
 public class ChangePasswordCommand
 {
-    public string CurrentPassword { get; set; }
-    public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
+    [Required(ErrorMessage = "رمز عبور فعلی ضروری است")]
+    public string CurrentPassword { get; set; } = null!;
+
+    [Required(ErrorMessage = "رمز عبور جدید ضروری است")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "رمز عبور باید حداقل ۶ کاراکتر و حداکثر ۱۰۰ کاراکتر باشد")]
+    public string Password { get; set; } = null!;
+
+    [Required(ErrorMessage = "تأیید رمز عبور ضروری است")]
+    [Compare("Password", ErrorMessage = "رمز عبور و تأیید رمز عبور مطابقت ندارند")]
+    public string ConfirmPassword { get; set; } = null!;
 }
