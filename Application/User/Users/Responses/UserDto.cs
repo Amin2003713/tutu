01c12784facdc8a9c3bc1c8d955213dfbc88de1b
amin <amin1382422amin@gmail.com>
2024-10-16 17:31:32 +0330

    hgckgjh

--- Application/User/Users/Responses/UserDto.cs
+++ Application/User/Users/Responses/UserDto.cs
@@ -22,9 +22,9 @@ public class UserDto : BaseDto
         var claimIdentityList = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, userInfo.Id.ToString()),
-            new(ClaimTypes.Name, userInfo.PhoneNumber),
+            new(ClaimTypes.MobilePhone, userInfo.PhoneNumber),
             new(ClaimTypes.Surname, userInfo.Family),
-            new(ClaimTypes.GivenName, userInfo.Name),
+            new(ClaimTypes.Name, userInfo.Name),
             new(ClaimTypes.Email, userInfo.Email ?? "@"),
             new(ClaimTypes.Gender, userInfo.Gender.ToString()),
             new(ClaimTypes.UserData, userInfo.AvatarName)
@@ -36,42 +36,40 @@ public class UserDto : BaseDto
 
     public static UserDto FromPrincipal(ClaimsPrincipal principal)
     {
-        if (principal.Identity != null && principal.Identity.IsAuthenticated)
+        if (principal.Identity == null || !principal.Identity.IsAuthenticated)
+            return new UserDto();
+
+        var userId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var email = principal.FindFirst(ClaimTypes.Email)?.Value;
+        var family = principal.FindFirst(ClaimTypes.Surname)?.Value;
+        var name = principal.FindFirst(ClaimTypes.Name)?.Value;
+        var gender = principal.FindFirst(ClaimTypes.Gender)?.Value;
+        var avatarName = principal.FindFirst(ClaimTypes.UserData)?.Value;
+        var phoneNumber =
+            principal.FindFirst(ClaimTypes.MobilePhone)?.Value; // Assuming phone number is stored in the 'Name' claim
+    
+        var roles = principal.FindAll(ClaimTypes.Role).Select(r => r.Value).Select(a => new UserRoleDto
         {
-            var userId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var email = principal.FindFirst(ClaimTypes.Email)?.Value;
-            var family = principal.FindFirst(ClaimTypes.Surname)?.Value;
-            var name = principal.FindFirst(ClaimTypes.GivenName)?.Value;
-            var gender = principal.FindFirst(ClaimTypes.Gender)?.Value;
-            var avatarName = principal.FindFirst(ClaimTypes.UserData)?.Value;
-            var phoneNumber =
-                principal.FindFirst(ClaimTypes.Name)?.Value; // Assuming phone number is stored in the 'Name' claim
-            // You might need to adjust these to map the correct claims.
+            RoleId = Convert.ToInt64(a.Split("#").First()),
+            RoleTitle = a.Split("#").Last()
+        }).ToList();
 
-            // Assuming roles are stored in claims
-            var roles = principal.FindAll(ClaimTypes.Role).Select(r => r.Value).Select(a => new UserRoleDto
+        // Now you can populate the UserDto
+        if (userId != null)
+            return new UserDto
             {
-                RoleId = Convert.ToInt64(a.Split("#").First()),
-                RoleTitle = a.Split("#").Last()
-            }).ToList();
-
-            // Now you can populate the UserDto
-            if (userId != null)
-                return new UserDto
-                {
-                    Id = Convert.ToInt64(userId),
-                    Email = email ?? "@", // Use fallback for null values
-                    Family = family,
-                    Gender = Enum.Parse<Gender>(gender!),
-                    Name = name,
-                    Roles = roles,
-                    AvatarName = avatarName,
-                    PhoneNumber = phoneNumber,
-                    Password = string.Empty, // Not available from claims
-                    CreationDate = DateTime.Now, // Adjust if creation date needs to come from elsewhere
-                    IsActive = true // Adjust based on your application logic
-                };
-        }
+                Id = Convert.ToInt64(userId),
+                Email = email ?? "@", // Use fallback for null values
+                Family = family!,
+                Gender = Enum.Parse<Gender>(gender!),
+                Name = name!,
+                Roles = roles,
+                AvatarName = avatarName!,
+                PhoneNumber = phoneNumber!,
+                Password = string.Empty, // Not available from claims
+                CreationDate = DateTime.Now, // Adjust if creation date needs to come from elsewhere
+                IsActive = true // Adjust based on your application logic
+            };
 
         return new UserDto();
     }
--- MyShop.Ui/Extensions/ClaimsPrincipalExtensions.cs
+++ MyShop.Ui/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,10 +1,11 @@
 ﻿using System.Security.Claims;
+using Application.User.Users.Responses;
 
 namespace MyShop.Ui.Extensions;
 
 internal static class ClaimsPrincipalExtensions
 {
-    internal static string GetEmail(this ClaimsPrincipal claimsPrincipal)
+    internal static string Email(this ClaimsPrincipal claimsPrincipal)
     {
         return claimsPrincipal.FindFirst(ClaimTypes.Email)!.Value;
     }
@@ -16,22 +17,31 @@ internal static class ClaimsPrincipalExtensions
 
     internal static string FullName(this ClaimsPrincipal claimsPrincipal)
     {
-        return claimsPrincipal.FindFirst(ClaimTypes.GivenName)!.Value +" "+
+        return claimsPrincipal.FindFirst(ClaimTypes.Name)!.Value +" "+
                claimsPrincipal.FindFirst(ClaimTypes.Surname)!.Value;
     }
 
-    internal static string GetLastName(this ClaimsPrincipal claimsPrincipal)
+    internal static string LastName(this ClaimsPrincipal claimsPrincipal)
     {
         return claimsPrincipal.FindFirst(ClaimTypes.Surname)!.Value;
     }
 
-    internal static string GetPhoneNumber(this ClaimsPrincipal claimsPrincipal)
+    internal static string PhoneNumber(this ClaimsPrincipal claimsPrincipal)
     {
         return claimsPrincipal.FindFirst(ClaimTypes.MobilePhone)!.Value;
     }
 
-    internal static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+    internal static string UserId(this ClaimsPrincipal claimsPrincipal)
     {
         return claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+    }
+    internal static string Avatar(this ClaimsPrincipal claimsPrincipal)
+    {
+        return claimsPrincipal.FindFirst(ClaimTypes.UserData)!.Value;
+    }
+
+    internal static string Gender(this ClaimsPrincipal claimsPrincipal)
+    {
+        return claimsPrincipal.FindFirst(ClaimTypes.Gender)!.Value;
     }
 }
